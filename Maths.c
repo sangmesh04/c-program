0fda2333c8f2ee2be25d57851cbb99d4981ee94c
sangmesh04 <78370367+sangmesh04@users.noreply.github.com>
2021-03-20 19:34:16 +0530

    c programme

---
+++ Maths.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	float f;
+	float A;
+	printf("Enter the side of the square:",f);
+	scanf("%f",&f);
+	
+	A= (f*f);
+	
+	printf("The area of the square is %f",A,f);
+	
+	return 0;
+}
